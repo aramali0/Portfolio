e2bcca538c594a6d3ae5ab49134b6e29bf106eab
Aramali Mohammed <mouhammedaramali@gmail.com>
2024-11-05 09:07:54 +0100

    add the vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
-
+import { Analytics } from "@vercel/analytics/react"
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import Education from "./components/Education";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Analytics  />
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
